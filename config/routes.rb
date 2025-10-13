7d9579fe93a009ae9c829c59e8a1f3b93153f8a2
AndreiCostinOlaru <andrei_olaru9@yahoo.ro>
2025-10-13 15:29:11 +0300

    Edit and update blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  patch "/blog_posts/:id", to: "blog_posts#update"
+  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
+
   post "/blog_posts", to: "blog_posts#create", as: :blog_posts
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
