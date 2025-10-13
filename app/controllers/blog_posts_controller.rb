4c3e6833975b0d30d13b5964bf170a5da392db43
AndreiCostinOlaru <andrei_olaru9@yahoo.ro>
2025-10-13 16:33:02 +0300

    Add delete button to edit form

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -35,6 +35,13 @@ class BlogPostsController < ApplicationController
     end
   end
 
+  def destroy
+    @blog_post = BlogPost.find(params[:id])
+    if @blog_post.destroy
+      redirect_to root_path
+    end
+  end
+
   def blog_post_params
     params.require(:blog_post).permit(:title, :body)
   end
